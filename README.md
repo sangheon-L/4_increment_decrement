628b9b3247fb8457358fd736bc5f0c9f62819fce
snagheon-L <tkdgjs0107@naver.com>
2021-04-22 17:18:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 4_increment_decrement
+ 4.6_javascript
---
+++ js/common.js
@@ -0,0 +1,14 @@
+// /*변수선언*/
+// let numberNum = 10;
+// alert(numberNum++); //다음 값을 하나 증가시켜서 띄워라 의미
+// alert(numberNum);
+//
+
+/*변수선언*/
+let numberNum = 10;
+alert(numberNum++); //다음 값을 하나 증가시켜서 띄워라 의미
+alert(--numberNum);
+alert(++numberNum);
+alert(numberNum--);
+alert(numberNum--);
+alert(++numberNum);
